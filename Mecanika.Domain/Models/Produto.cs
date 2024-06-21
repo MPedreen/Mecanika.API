22670c3b419818fd6f8511713c5f842f97e260aa
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:53 -0300

    feat: criacao model Produto

---
+++ Mecanika.Domain/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+        public int FornecedorId { get; set; }
+    }
+}
