72a3ff36db7991c6883092d63f59a2f404e8b201
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:10 -0300

    feat: criacao model Estoque

---
+++ Mecanika.Domain/Models/Estoque.cs
@@ -0,0 +1,12 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Estoque
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public DateTime? DataSaida { get; set; }
+        public int EstabelecimentoId { get; set; }
+    }
+}
