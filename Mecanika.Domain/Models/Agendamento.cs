f67b33df444baf7196df684c45573b754a233e41
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:32:26 -0300

    feat: criacao model Agendamento

---
+++ Mecanika.Domain/Models/Agendamento.cs
@@ -0,0 +1,13 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan Hora { get; set; }
+        public string Status { get; set; }
+        public int ClienteId { get; set; }
+        public int ServicoId { get; set; }
+        public int FuncionarioId { get; set; }
+    }
+}
