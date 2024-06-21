f7e50faad184228900a3b19ad953deb09419f8cc
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:23 -0300

    feat: criacao model Fornecedor

---
+++ Mecanika.Domain/Models/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+    }
+}
