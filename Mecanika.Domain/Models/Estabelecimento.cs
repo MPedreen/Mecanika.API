f363cf0041d92f055247ff7bd71ec96ceaadb4a8
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:02 -0300

    feat: criacao model Estabelecimento

---
+++ Mecanika.Domain/Models/Estabelecimento.cs
@@ -0,0 +1,17 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Estabelecimento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Email { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+    }
+}
