12c2fb8d144698f353ac7e5d794cf0beaae17192
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:46 -0300

    feat: criacao model Veiculo

---
+++ Mecanika.Domain/Models/Veiculo.cs
@@ -0,0 +1,14 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+        public string TipoVeiculo { get; set; }
+        public int ClienteId { get; set; }
+    }
+}
