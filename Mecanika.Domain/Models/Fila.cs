d0ed5c49b69f92a0e80f3b30f55b6067cc05c411
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:16 -0300

    feat: criacao model Fila

---
+++ Mecanika.Domain/Models/Fila.cs
@@ -0,0 +1,12 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Fila
+    {
+        public int Id { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public DateTime? DataSaida { get; set; }
+        public int NumeroFila { get; set; }
+        public int ClienteId { get; set; }
+        public int ServicoId { get; set; }
+    }
+}
