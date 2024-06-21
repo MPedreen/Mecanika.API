7166c89f106458c3afa4bd5d338fe8030fffa27f
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:37 -0300

    feat: criacao model Servico

---
+++ Mecanika.Domain/Models/Servico.cs
@@ -0,0 +1,11 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal Duracao { get; set; }
+    }
+}
