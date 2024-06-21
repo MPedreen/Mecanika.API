d76bb508a0479ae08282cb6c67dbab080d971b67
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-20 22:33:31 -0300

    feat: criacao model Funcionario

---
+++ Mecanika.Domain/Models/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿namespace Mecanika.Domain.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public char Genero { get; set; }
+        public string Email { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+        public string Cargo { get; set; }
+        public int EstabelecimentoId { get; set; }
+    }
+}
