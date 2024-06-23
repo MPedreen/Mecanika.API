afeb8f1fb0c10506a833d1885074e2a7a1313e95
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-23 16:47:57 -0300

    feat: criacao ServiceConfiguration

---
+++ Mecanika.Infra.Data/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Mecanika.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mecanika.Infra.Data.Configurations
+{
+    public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+    {
+        public void Configure(EntityTypeBuilder<Service> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Name).HasMaxLength(100).IsRequired();
+            builder.Property(s => s.Description).HasMaxLength(200).IsRequired();
+            builder.Property(s => s.Price).HasPrecision(10, 2).IsRequired();
+            builder.Property(s => s.Duration).HasPrecision(5, 2).IsRequired();
+
+            builder.HasMany(s => s.Schedulings).WithOne(sch => sch.Service).HasForeignKey(sch => sch.ServiceId).OnDelete(DeleteBehavior.Restrict);
+            builder.HasMany(s => s.Queues).WithOne(q => q.Service).HasForeignKey(q => q.ServiceId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
