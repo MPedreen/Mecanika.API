ad160b451206d424d674779e4adade6d260bcd0d
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-23 22:54:59 -0300

    feat: criacao camada DependencyInjectionAPI

---
+++ Mecanika.Infra.IoC/DependencyInjectionAPI/DependencyInjectionAPI.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mecanika.Infra.IoC.DependencyInjectionAPI
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
