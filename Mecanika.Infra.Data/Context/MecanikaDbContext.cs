2f164a71517195b45bf748bf4f7fa5aa657b9c51
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-23 22:55:21 -0300

    feat: criacao DbContext com mapeamento DbSets

---
+++ Mecanika.Infra.Data/Context/MecanikaDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Mecanika.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mecanika.Infra.Data.Context
+{
+    public class MecanikaDbContext : IdentityDbContext<IdentityUser>
+    {
+        public MecanikaDbContext(DbContextOptions<MecanikaDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Establishment> Establishments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Queue> Queues { get; set; }
+        public DbSet<Scheduling> Schedulings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(MecanikaDbContext).Assembly);
+        }
+    }
+}
