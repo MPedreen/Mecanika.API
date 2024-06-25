a9333f3d5117c3616546bdab9d4397a97b555bb3
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-06-25 20:14:10 -0300

    fix: corrigindo tipo Queue Id para Guid

--- Mecanika.Domain/Models/Queue.cs
+++ Mecanika.Domain/Models/Queue.cs
@@ -2,12 +2,12 @@
 
 public class Queue
 {
-    public int Id { get; private set; }
+    public Guid Id { get; private set; }
     public DateTime DateEntry { get; private set; }
     public int Total { get; private set; }
     public virtual ICollection<Scheduling>? Schedulings { get; private set; }
 
-    public Queue(int id, DateTime dateEntry, int total)
+    public Queue(Guid id, DateTime dateEntry, int total)
     {
         Id = id;
         DateEntry = dateEntry;
