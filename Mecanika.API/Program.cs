74f8d3dc49e8523966a7686229eb8c107bbbf68f
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-07-16 20:26:44 -0300

    feat: chamando AddInfrasctrutureAPI na Program

--- Mecanika.API/Program.cs
+++ Mecanika.API/Program.cs
@@ -1,8 +1,11 @@
+using Mecanika.Infra.IoC.DependencyInjectionAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureAPI(builder.Configuration);
 
 var app = builder.Build();
 
