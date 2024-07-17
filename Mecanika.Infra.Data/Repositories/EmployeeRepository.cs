b795478d06ec76f6362636f57241748cc8b644d3
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-07-16 22:17:12 -0300

    feat: criacao implementacao EmployeeRepository

---
+++ Mecanika.Infra.Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mecanika.Domain.Interfaces.Repositories;
+using Mecanika.Domain.Models;
+using Mecanika.Infra.Data.Context.SQL;
+
+namespace Mecanika.Infra.Data.Repositories
+{
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(MecanikaDbContext context) : base(context)
+        {
+
+        }
+    }
+}
