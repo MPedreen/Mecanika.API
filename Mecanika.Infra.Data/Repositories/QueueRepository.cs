c6b04873b8f8c8d355dfad04cc2ba6eda256333c
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-07-16 22:18:00 -0300

    feat: criacao implementacao QueueRepository

---
+++ Mecanika.Infra.Data/Repositories/QueueRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mecanika.Domain.Interfaces.Repositories;
+using Mecanika.Domain.Models;
+using Mecanika.Infra.Data.Context.SQL;
+
+namespace Mecanika.Infra.Data.Repositories
+{
+    public class QueueRepository : Repository<Queue>, IQueueRepository
+    {
+        public QueueRepository(MecanikaDbContext context) : base(context)
+        {
+
+        }
+    }
+}
