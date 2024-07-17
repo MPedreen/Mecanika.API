048a4a71b56af740c23403737814af5756688c64
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-07-16 22:17:01 -0300

    feat: criacao implementacao ClientRepository

---
+++ Mecanika.Infra.Data/Repositories/ClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mecanika.Domain.Interfaces.Repositories;
+using Mecanika.Infra.Data.Context.SQL;
+
+namespace Mecanika.Infra.Data.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        public ClientRepository(MecanikaDbContext context)
+        {
+
+        }
+    }
+}
