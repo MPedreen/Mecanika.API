1c0ee6d8820cdc01f307761f899bd69d2040fc4c
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-07-16 22:20:47 -0300

    feat: criacao implementacao SupplierRepository

---
+++ Mecanika.Infra.Data/Repositories/SupplierRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mecanika.Domain.Interfaces.Repositories;
+using Mecanika.Domain.Models;
+using Mecanika.Infra.Data.Context.SQL;
+
+namespace Mecanika.Infra.Data.Repositories
+{
+    public class SupplierRepository : Repository<Supplier>, ISupplierRepository
+    {
+        public SupplierRepository(MecanikaDbContext context) : base(context)
+        {
+
+        }
+    }
+}
