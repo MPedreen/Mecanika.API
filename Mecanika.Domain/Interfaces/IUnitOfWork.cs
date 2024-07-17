975772fdc2fedfdfeee93a41324bbe477f54dfcf
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-07-16 22:15:57 -0300

    feat: criacao contrato IUnitOfWork

---
+++ Mecanika.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Mecanika.Domain.Interfaces.Repositories;
+
+namespace Mecanika.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IClientRepository Clients { get; }
+        IEmployeeRepository Employees { get; }
+        IEstablishmentRepository Establishments { get; }
+        IProductRepository Products { get; }
+        IQueueRepository Queues { get; }
+        ISchedulingRepository Schedulings { get; }
+        IServiceRepository Services { get; }
+        IStockRepository Stocks { get; }
+        ISupplierRepository Suppliers { get; }
+        IVehicleRepository Vehicles { get; }
+        int Complete();
+    }
+}
